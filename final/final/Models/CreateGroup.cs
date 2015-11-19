76c900f42e8888eeb07a1188b079a57420cee1e0
Tomer Aronovsky <tomer_aronovsky@hotmail.com>
2015-11-19 14:47:57 +0200

    CreateGroup Model

---
+++ final/final/Models/CreateGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace final.Models
+{
+    public class CreateGroup
+    {
+        public List<Product> products { get; set; }
+        public List<Category> categories { get; set; }
+        public List <SubCategories> subCategories { get; set; }
+
+        public CreateGroup()
+        {
+            this.products = new List<Product>();
+            
+        }
+    }
+}
