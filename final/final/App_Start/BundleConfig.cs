09f9578718e2988e180eef14c21ea86489a99dcf
Tomer Aronovsky <tomer_aronovsky@hotmail.com>
2015-12-17 14:20:35 +0200

    Hapoel Kfs since 1928

--- final/final/App_Start/BundleConfig.cs
+++ final/final/App_Start/BundleConfig.cs
@@ -14,6 +14,13 @@ namespace final
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/js/jquery.poptrox.min.js",
+                        "~/Scripts/js/skel-viewport.min.js",
+                        "~/Scripts/js/util.js",
+                        "~/Scripts/js/main.js"));
+
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -23,9 +30,13 @@ namespace final
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/css/main.css"));
+
+            
         }
     }
 }
